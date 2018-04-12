eeefbd27158fab2732384dbfe46a78b5b187d8cd
superkirill <sirotkinkirill@yeandex.ru>
2018-04-12 21:33:54 +0700

    Implementation - get_chord (Test passed)

---
+++ app/generator.py
@@ -0,0 +1,11 @@
+class Generator():
+    """
+        Generates chords and melodies
+    """
+    pass
+
+    def get_chord(self):
+        """
+            Generate a chord
+        """
+        return (1, 2, 3)
