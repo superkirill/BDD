2b6f4a8f65b7827a58ea7a1d785f5a449196e246
superkirill <sirotkinkirill@yeandex.ru>
2018-04-12 23:01:32 +0700

    Test rewritten - step_generator function fixed (failed)

--- tests/features/steps/step_generator.py
+++ tests/features/steps/step_generator.py
@@ -7,7 +7,11 @@ def step_impl(context):
 
 @when('the chord {chord} is requested')
 def step_impl(context, chord):
-    context.chord = context.generator.get_chord(str(chord))
+    chord = chord.split(', ')
+    if len(chord) == 2:
+        context.chord = context.generator.get_chord(chord[0], chord[1])
+    else:
+        context.chord = context.generator.get_chord(chord[0])
 
 @then('Generator returns {notes}')
 def step_impl(context, notes):
